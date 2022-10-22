3ce7d39da6e763719ac69d9fac8c2bb946df89c5
gleon <gleonnyc@gmail.com>
2022-10-21 21:41:34 -0400

    commit as of 10/21

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,9 +3,14 @@
 
 function About() {
     return(
-    <dive className="About Us">
+    <div className="About Us">
+        <h1 class=' mt-24 pt-24 w-80 text-6xl text-center'> ABOUT US</h1>
+          <h2 class='pt-5 text-center text-lg'>Here at FIU we are given the opportunity to work on new and exciting. 
+          P2P Drive is a the perfect opportunity to enhance our skills and enhance our development. 
+          This project focuses on creating a decentralized file sharing system, 
+          storing fragments of your personalized files accross multiple nodes.</h2>
         
-    </dive>
+    </div>
     )
 }
 
