2de93924e3a5050f445a4da0042f5512bc685def
Johnny L <jller023@fiu.edu>
2022-11-11 03:46:13 -0500

    Made Home page mobile friendly

---
+++ Client/src/Components/Info.jsx
@@ -0,0 +1,11 @@
+
+function Info () {
+    return(
+        <div className="Info">
+            
+            <h1>This is info</h1>
+        </div>
+    )
+}
+
+export default Info;
--- Client/src/Components/Layout.jsx
+++ Client/src/Components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import  Navbar from "../Navigation/Navbar";
+import Navbar from '../Navigation/Navbar';
+
 
 
 const Layout = () => {
