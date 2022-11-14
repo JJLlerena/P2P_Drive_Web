95393351b0fed6fe2b1969e3ddae846b3eef3518
gleon <gleonnyc@gmail.com>
2022-11-14 11:29:31 -0500

    added names to about

--- Client/src/Components/About.jsx
+++ Client/src/Components/About.jsx
@@ -1,17 +1,62 @@
 import {UserCircleIcon} from "@heroicons/react/24/outline";
-import Screen from "../images/Screen.png";
 function About() {
     return(
     <div className="About">
         <h1 class='mt-10 w-86 text-4xl text-center pb-5'>ABOUT US</h1>
-          <p class='w-auto px-auto mx-32 text-xl text-center'>Here at FIU we are given the opportunity to work on new and exciting. 
-          P2P Drive is a the perfect opportunity to enhance our skills and enhance our development. 
+          <p class='w-auto px-auto pb-5 mx-10 text-xl text-center md:w-80 md:mx-auto'>Here at FIU we are given the opportunity to work on new and exciting. 
+          P2P Drive is the perfect opportunity to enhance our skills and enhance our development. 
           This project focuses on creating a decentralized file sharing system, 
           storing fragments of your personalized files accross multiple nodes.</p>
-          <div class='box-border h-32 w-96 px-auto border-4 mx-auto'>
-               <h1 class="">Good</h1>
-               <UserCircleIcon className="h-6 w-6"src={Screen}/>
-
+          <div class='grid bg-slate-200 pt-5 gap-y-4 grid-cols-2 h-fit w-96 px-10 mx-auto justify-items-center'>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Johnny Llerena
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                   Gerardo Leon 
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Michael Garrido
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Martin Moreano
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Keiss De Los Santos
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Shawn Zukowski
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Jorge Cortes
+                </span>
+            </div>
+            <div>
+                <UserCircleIcon className="h-10 w-10"/>
+                <span>
+                    Hello
+                </span>
+            </div>
+             
           </div>
         
     </div>
