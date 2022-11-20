5ca626796bc65f08861c9aa4a152c0bc20503b09
gleon <gleonnyc@gmail.com>
2022-11-19 21:04:26 -0500

    images added

--- Client/src/Components/About.jsx
+++ Client/src/Components/About.jsx
@@ -1,15 +1,22 @@
 import {UserCircleIcon} from "@heroicons/react/24/outline";
+import Patrick from "../images/Patrick.webp";
+import Penguin from "../images/Johnnypfp.webp";
+import Gerardo from "../images/Gerardopfp.webp";
+import Michael from "../images/Michaelpfp.png";
+import Squidward from "../images/Shawnpfp.webp";
+import Jorge from "../images/Jorgepfp.webp";
+import DOM from "../images/Dominickpfp.webp";
 function About() {
     return(
     <div className="About">
         <h1 class='mt-10 w-86 text-4xl text-center pb-5'>ABOUT US</h1>
-          <p class='w-auto px-auto pb-5 mx-10 text-xl text-center md:w-80 md:mx-auto'>Here at FIU we are given the opportunity to work on new and exciting. 
+          <p class='w-auto px-auto pb-5 mx-10 text-xl text-center md:w-fit md:px-40 md:mx-auto'>Here at FIU we are given the opportunity to work on new and exciting. 
           P2P Drive is the perfect opportunity to enhance our skills and enhance our development. 
           This project focuses on creating a decentralized file sharing system, 
           storing fragments of your personalized files accross multiple nodes.</p>
-          <div class='grid bg-slate-200 pt-5 gap-y-4 grid-cols-2 h-fit w-96 px-10 mx-auto justify-items-center'>
+          <div class='grid bg-slate-200 pt-5 gap-y-4 grid-cols-2 h-fit w-96 px-10 mx-auto'>
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={Penguin}/>
                 <span>
                     Johnny Llerena
                 </span>
@@ -19,7 +26,7 @@ function About() {
                 </span>
             </div>
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={Gerardo}/>
                 <span>
                     Gerardo Leon
                 </span>
@@ -29,7 +36,7 @@ function About() {
                 </span>
             </div>
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={Michael}/>
                 <span>
                     Michael Garrido
                 </span>
@@ -39,7 +46,7 @@ function About() {
                 </span>
             </div>
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={Patrick}/>
                 <span>
                     Martin Moreano
                 </span>
@@ -48,18 +55,9 @@ function About() {
                     Back End Developer
                 </span>
             </div>
+            
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
-                <span>
-                    Keiss De Los Santos
-                </span>
-                <br/>
-                <span>
-                    Back End Developer
-                </span>
-            </div>
-            <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={Squidward}/>
                 <span>
                     Shawn Zukowski
                 </span>
@@ -69,7 +67,7 @@ function About() {
                 </span>
             </div>
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={Jorge}/>
                 <span>
                     Jorge Cortes
                 </span>
@@ -79,7 +77,7 @@ function About() {
                 </span>
             </div>
             <div>
-                <UserCircleIcon className="h-10 w-10"/>
+            <img class='h-10 w-10 rounded-full justify-self-center mx-10' src={DOM}/>
                 <span>
                     Dominick Diaz
                 </span>
