f8c1f33417d62e69a58466059e0753052692a423
Johnny L <jller023@fiu.edu>
2022-11-28 01:04:29 -0500

    added Dashboard

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
-import Home from './Components/Home';
+import LaunchPage from './Components/Launchpage';
 import Info from './Components/Info';
 import About from './Components/About';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
+import Home from './Dashboard/Home';
+import Files from './Dashboard/Files';
+import Explore from './Dashboard/Explore';
+import Settings from './Dashboard/Settings';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,12 +21,19 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<LaunchPage />} />
           <Route path="info" element={<Info />} />
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="Dashboards" element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} >
+              <Route index element={<Home />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="files" element={<Files />} />
+              <Route path="explore" element={<Explore />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="*" element={<NoPage />} />
+          </Route>
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
