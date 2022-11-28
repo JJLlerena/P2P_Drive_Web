571da07653f2b605ad2e29c49e4f33138f0774e1
Johnny L <jller023@fiu.edu>
2022-11-28 04:12:25 -0500

    ll

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+import LaunchPage from './Components/Launchpage';
+import Info from './Components/Info';
+import About from './Components/About';
+import Register from './Components/Register';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
+import Home from './Dashboard/Home';
+import Files from './Dashboard/Files';
+import Explore from './Dashboard/Explore';
+import Settings from './Dashboard/Settings';
+
+<React.StrictMode>
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<LaunchPage />} />
+      <Route path="info" element={<Info />} />
+      <Route path="about" element={<About />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="dashboard" element={<Dashboard />} >
+          <Route index element={<Home />} />
+          <Route path="Home" element={<Home />} />
+          <Route path="files" element={<Files />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<NoPage />} />
+      </Route>
+      <Route path="*" element={<NoPage />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+</React.StrictMode>
+);
+
+
+function NoPage() {
+return(
+<div>
+  <h2>404 Page not found</h2>
+</div>
+)
+}
